5c9997ffb57b9412f6be234f474b0aa3a6b1ad9e
Eslam <eslamm378@gmail.com>
2025-07-24 22:04:35 +0300

    Add core functionality for Student Management System
    Introduced the `Student` data class, `Action` sealed class for managing actions, and a basic menu-driven interface in `main`.

---
+++ src/main/kotlin/Action.kt
@@ -0,0 +1,11 @@
+package org.example
+
+sealed class Action {
+    data class AddStudent(val student: Student) : Action()
+    class ViewAllStudents() : Action()
+    data class FilterByGrade(val grade: Double) : Action()
+    data class FilterByStatus(val status: String) : Action()
+    data class FilterByName(val name: String) : Action()
+    data class UpdateStudent(val id: Int) : Action()
+    data class RemoveStudent(val id: Int) : Action()
+}
