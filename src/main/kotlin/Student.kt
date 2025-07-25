c13cdc0ebbeeb7f46d136ddb2d7476b4dc7cfdff
Eslam <eslamm378@gmail.com>
2025-07-25 15:40:18 +0300

    Refactor `Student` class to auto-generate IDs and simplify `getStudentDetails` logic

--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -1,10 +1,18 @@
 package org.example
 
 data class Student(
-    val id: Int,
     var name: String,
     var gpa: Double? = null,
     var grade: String? = null,
     var status: String? = null,
     var notes: MutableList<String>? = null
-)
+) {
+    val id: Int = generateId()
+
+    companion object {
+        private var id = 0
+        fun generateId(): Int {
+            return ++id
+        }
+    }
+}
