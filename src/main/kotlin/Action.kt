7ddf53e7db1f9665c4c7d736f561b40baec69539
shorouksaffan <shorouk.saffan2005@gmail.com>
2025-07-25 21:05:00 +0300

    Update Action.kt

--- src/main/kotlin/Action.kt
+++ src/main/kotlin/Action.kt
@@ -3,6 +3,7 @@ package org.example
 sealed class Action {
     object AddStudent : Action()
     object ViewAllStudents : Action()
+    data class ViewStudent(val id: Int) : Action()  
     data class FilterByGrade(val grade: String) : Action()
     data class FilterByStatus(val status: String) : Action()
     data class FilterByName(val name: String) : Action()
