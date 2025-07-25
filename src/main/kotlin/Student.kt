2bb0a1de0bc94af7b302ad7afeb6c3ca9e383e07
m7mdfox <fcb.mohamed2005@gmail.com>
2025-07-25 16:27:43 +0300

    made the grade calculated from the gpa

--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -2,17 +2,27 @@ package org.example
 
 data class Student(
     var name: String,
-    var gpa: Double? = null,
-    var grade: String? = null,
+    var gpa: Double ,
     var status: String? = null,
     var notes: MutableList<String>? = null
 ) {
     val id: Int = generateId()
+    var grade: String? = calculateGrade(gpa)
 
     companion object {
         private var id = 0
         fun generateId(): Int {
             return ++id
+        }
+        fun calculateGrade(gpa: Double): String? {
+            when {
+                gpa >= 3.5 -> return "A"
+                gpa >= 3 -> return "B"
+                gpa >= 2.5 -> return "C"
+                gpa >= 2 -> return "D"
+                gpa == 0.0 -> return "Undefined"
+                else -> return "F"
+            }
         }
     }
 }
